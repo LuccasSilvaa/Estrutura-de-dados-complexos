feb09cb0a2abf54631685682447c0b9a40bf1477
LuccasSilvaa <correaluccas@hotmail.com>
2022-07-08 20:57:31 -0300

    subindo atividade Estrutura de dados complexos

---
+++ script.js
@@ -0,0 +1,27 @@
+// Crie um array que receba 5 itens e exiba no console.
+let alunos = ["Kelly", "Limão", "Luccas","Pamela", "Rafael"]
+console.log(alunos);
+console.log(alunos[2]);
+
+ //Utilize um método para adicionar um nome ao inicio do array.
+alunos.unshift("Tulani");
+console.log("adicionando nome no array", alunos);
+
+//Utilize um método para remover o último nome do array.
+alunos.pop();
+console.log("atualizão no array", alunos);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+alunos.pop();
+console.log("atualizão no array", alunos);
+
+// Utilize um método para remover o primeiro nome do array.
+alunos.pop();
+console.log("atualizão no array", alunos);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numeros = [7,5,6,3,8,9,2,1,4];
+console.log(numeros);
+
+numeros.sort();
+console.log(numeros);
